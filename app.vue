5b9458e84279b7d805ce9f2cab366f6bd2b29612
eduvarjaor <eduardovarjaor@gmail.com>
2024-01-08 13:04:23 -0300

    feat(App): import Header

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div class="bg-slate-100 min-h-[100vh]">
+        <Header />
+    </div>
 </template>
+
+<script>
+import Header from "./src/components/Header.vue";
+
+export default {
+    components: {
+        Header,
+    },
+};
+</script>
