48aa6bcecd10cdf927b09a0526af47a52b512509
eduvarjaor <eduardovarjaor@gmail.com>
2024-01-08 16:25:23 -0300

    feat(App): added SearchPanel

--- app.vue
+++ app.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="bg-slate-100 min-h-[100vh]">
         <Header />
+        <SearchPanel />
     </div>
 </template>
 
 <script>
 import Header from "./src/components/Header.vue";
+import SearchPanel from "./src/components/SearchPanel.vue";
 
 export default {
     components: {
         Header,
+        SearchPanel,
     },
 };
 </script>
