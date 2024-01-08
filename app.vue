fb0b5ef0f4ae6cab796cd2718e8fd9f7df2a4e53
eduvarjaor <eduardovarjaor@gmail.com>
2024-01-08 16:36:11 -0300

    feat(App): added TransactionDisplay

--- app.vue
+++ app.vue
@@ -2,17 +2,20 @@
     <div class="bg-slate-100 min-h-[100vh]">
         <Header />
         <SearchPanel />
+        <TransactionDisplay />
     </div>
 </template>
 
 <script>
 import Header from "./src/components/Header.vue";
 import SearchPanel from "./src/components/SearchPanel.vue";
+import TransactionDisplay from "./src/components/TransactionDisplay.vue";
 
 export default {
     components: {
         Header,
         SearchPanel,
+        TransactionDisplay,
     },
 };
 </script>
